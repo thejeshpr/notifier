f67fe58cdfd6b6394720fc1c2433f9c7fd72b224
Thejesh PR <tp-r@microfocus.com>
2020-08-26 12:16:48 +0530

    updated ng

--- custom_lib/ng.py
+++ custom_lib/ng.py
@@ -3,12 +3,15 @@ import os
 from pprint import pprint
 import time
 import traceback
-from urllib.request import urlopen, Request, ProxyHandler, build_opener
+# from urllib.request import urlopen, Request, ProxyHandler, build_opener
+import requests
 
 from .air_post import AirPost
 from .TelePusher import TelePusher
 
 
+PHANTOM_URL = os.environ.get("PHANTOM_URL")
+PHANTOM_KEY = os.environ.get("PHANTOM_KEY")
 BASE_URL = os.environ.get("NG_BASE_URL")
 TELEGRAM_CHANNEL = os.environ.get("NG_TELEGRAM_CHANNEL")
 
@@ -47,26 +50,17 @@ class Ng(object):
     def send_latest_posts(self):        
         pusher = TelePusher(chat_id=TELEGRAM_CHANNEL)
         try:
-            url = BASE_URL.format(grp=self.grp, typ=self.typ)                        
-            headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-            
-            proxies = {                
-                'http': os.environ.get('NG_PROXY'),                
-                'https': os.environ.get('NG_PROXY')                
-                }
-            proxy_handler = ProxyHandler(proxies)
-            # req = Request(url=url, headers=headers) 
+            d = {
+                "url": BASE_URL.format(grp=self.grp, typ=self.typ),
+                "renderType":"plainText",
+                "outputAsJson": "true"
+            }
 
-            opener = build_opener(proxy_handler)
-            response = Request(url, headers=headers)
-            res = opener.open(response)
+            url = PHANTOM_URL.format(key=PHANTOM_KEY)            
+            res = requests.post(url, data=json.dumps(d))            
             
-            # result = res.read()
-            # print(result)
-            
-            
-            if res.status in [200]:            
-                data = json.loads(res.read())
+            if res.status_code in [200]:            
+                data = res.json()
                 
                 posts = {}
                 
@@ -76,11 +70,14 @@ class Ng(object):
                 ap = AirPost("appqzdWspiYMEZxLy", "posts")
                 inserted_posts = ap.insert(posts, "id")
                 
-                pusher.send_message(f"Sending {len(inserted_posts)} posts of {self.grp}:{self.typ}")
+                no_of_posts = len(inserted_posts) 
+
+                pusher.send_message(f"Got {no_of_posts} posts of {self.grp}:{self.typ}")
                 time.sleep(5)
 
-                for post in inserted_posts:
-                    caption = "Title: {title}\n\nURL: {url}".format(title=post["title"], url=post["url"])
+                for idx, post in enumerate(inserted_posts):
+                    counter = f"{idx + 1}/{no_of_posts}"
+                    caption = "{ctr}\nTitle: {title}\n\nURL: {url}".format(ctr=counter, title=post["title"], url=post["url"])
                     if post["type"] == "Photo":
                         pusher.send_photo(photo_url=post["media_url"], caption=caption)
                     elif post["type"] == "Animated":
@@ -89,6 +86,6 @@ class Ng(object):
             else:
                 raise NgException(f"Invalid status code: {res.status}")
         except Exception as e:            
-            msg = f"An error occured while retriving the posts: {e}\n{traceback.format_exc()}"
-            print(msg)
+            msg = f"An error occured while retriving the posts: {e}"
+            print(traceback.format_exc())            
             pusher.send_message(text=msg)
