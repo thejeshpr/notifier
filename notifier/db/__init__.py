b3eb00e5abed6f586958cb842de42a6d78e2e10a
Thejesh PR <tp-r@microfocus.com>
2020-10-25 23:27:45 +0530

    added db params

--- notifier/db/__init__.py
+++ notifier/db/__init__.py
@@ -5,16 +5,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+from notifier import DEBUG
+
 DB_HOST = os.environ.get("DB_HOST")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 DB_PORT = os.environ.get("DB_PORT")
 
+if DEBUG:
+    SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN", "sqlite:///dev8.db?check_same_thread=False")
+else:
+    SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN", "sqlite:///dev8.db?check_same_thread=False")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -22,14 +26,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# only for SQLITE
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL
-# )
+if DEBUG:
+    # only for SQLITE
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 # async db cursor
