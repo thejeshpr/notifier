16a708bf36d33b3ad8a211a2a7687d1b5827a05e
Thejesh PR <tp-r@microfocus.com>
2020-10-27 19:44:30 +0530

    removed custom_lib

--- custom_lib/models.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional, Dict
-
-from pydantic import BaseModel
-
-
-class Payload(BaseModel):
-    url: str
-    headers: Optional[Dict[str, str]]
-    params: Optional[Dict[str, str]]
